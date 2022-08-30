dcf7edc25a00768c44675e0bd88ba4439d96dab3
RaquelSanntos <santos.soares0424@gmail.com>
2022-08-29 23:31:39 -0400

    Atualizando actvitys

---
+++ app/src/main/java/com/sortname/sorteio/SecondarySemesterActivity.java
@@ -0,0 +1,14 @@
+package com.sortname.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondarySemesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary_semester);
+    }
+}
