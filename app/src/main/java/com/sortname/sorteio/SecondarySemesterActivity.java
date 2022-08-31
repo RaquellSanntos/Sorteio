abe8ae02847eb0065a7b6843365fea35480f7e05
RaquelSanntos <santos.soares0424@gmail.com>
2022-08-30 22:03:04 -0400

    Criada classe de Alunos do segundo semestre

--- app/src/main/java/com/sortname/sorteio/SecondarySemesterActivity.java
+++ app/src/main/java/com/sortname/sorteio/SecondarySemesterActivity.java
@@ -3,6 +3,10 @@ package com.sortname.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class SecondarySemesterActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class SecondarySemesterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary_semester);
+    }
+    public void ganhadorSecundary(View view) {
+        String[] nomes = {
+                "BRENO DA SILVA ROCHA",
+                "CINTIA BRANDÃO DA SILVA",
+                "DANIEL BRUNO MEDEIROS",
+                "GUIBSON JUNIOR FERREIRA DOS SANTOS",
+                "JOÃO VITOR BARROS DA SIVA",
+                "JOSUÉ DOS SANTOS CALDAS",
+                "LEONARDO ABNER RIBEIRO",
+                "LUAN MACHADO DA FONSECA",
+                "JONATHAN SILVA SANTOS",
+                "MARIA EDUARDA SOUZA DOS SANTOS",
+                "NEG ELIAS ESPINO FERRER",
+                "RICKSON WILLIAM DANTAS FERREIRA",
+                "ROBERTO PARENTE DA SILVA",
+                "TALYSSON DE ABREU GARCIA",
+                "WASHINGTON SMITH ROCHA DA CUNHA"
+        };
+        int sorteado = new Random().nextInt(30);
+        TextView divulgarNome = findViewById(R.id.txtView);
+
+        divulgarNome.setText(nomes[sorteado]);
     }
 }
