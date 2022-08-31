0a88eabfd8d03093c21f08d7a60d49ea61821534
RaquelSanntos <santos.soares0424@gmail.com>
2022-08-31 00:30:04 -0400

    Tela Splash e Componentes de interfaces

---
+++ app/src/main/java/com/sortname/sorteio/ListPrimarySemesterActivity.java
@@ -0,0 +1,14 @@
+package com.sortname.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListPrimarySemesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_primary_semester);
+    }
+}
